5148115b5375adfeb37df680f365b9ac9f6bf366
locnx <locnx1999@gmail.com>
2026-09-05 12:20:41 +0700

    Add unit test for customer creation and fund transfer
    A new unit test, `Create_Customer_And_Transfer_Test`, was added to validate the process of creating customers, transferring funds between accounts, and verifying account balances and transactions.
    
    The test uses an in-memory SQLite database to simulate the database context and ensures the schema is created. It initializes two accounts with predefined balances, links them to respective customers, and performs a transfer operation.
    
    A helper method, `Create_Customer_And_Transfer_Arrange`, was introduced to handle the setup of customers and accounts. The test asserts the correctness of the transfer operation, account balances, and transaction records.

--- CoreBanking.UnitTests/CoreBankingUnitTests.cs
+++ CoreBanking.UnitTests/CoreBankingUnitTests.cs
@@ -103,5 +103,91 @@ namespace CoreBanking.UnitTests
             Assert.NotNull(transaction);
             Assert.Equal(depositRequest.Amount, transaction.Amount);
         }
+
+        [Fact]
+        public async Task Create_Customer_And_Transfer_Test()
+        {
+            // Arrange
+            var sqliteConnection = new SqliteConnection("data source=:memory:");
+            sqliteConnection.Open();
+            var dbContextOptions = new DbContextOptionsBuilder<CoreBankingDbContext>()
+                .UseSqlite(sqliteConnection).Options;
+            using var dbContext = new CoreBankingDbContext(dbContextOptions);
+            dbContext.Database.EnsureCreated();
+            var services = new CoreBankingServices(dbContext, NullLogger<CoreBankingServices>.Instance);
+
+            var accountSend = new Account
+            {
+                Id = Guid.NewGuid(),
+                Balance = 2000
+            };
+            var accountReceive = new Account
+            {
+                Id = Guid.NewGuid(),
+                Balance = 500
+            };
+
+            await Create_Customer_And_Transfer_Arrange(services, accountSend, accountReceive);
+
+            var transferRequest = new TransferRequest
+            {
+                Amount = 1000,
+                DestinationAccountNumber = accountReceive.Number
+            };
+
+            // act
+            var res = CoreBankingApi.Transfer(services, accountSend.Id, transferRequest);
+
+            // Assert
+            Assert.NotNull(res);
+
+            var updatedSendAccount = dbContext.Accounts.Find(accountSend.Id);
+            Assert.NotNull(updatedSendAccount);
+            Assert.Equal(accountSend.Balance - transferRequest.Amount, updatedSendAccount.Balance);
+
+            var updatedReceiveAccount = dbContext.Accounts.Find(accountReceive.Id);
+            Assert.NotNull(updatedReceiveAccount);
+            Assert.Equal(accountReceive.Balance + transferRequest.Amount, updatedReceiveAccount.Balance);
+
+            var transactionSend = dbContext.Transactions.FirstOrDefault(ts => ts.AccountId == accountSend.Id && ts.Type == TransactionTypes.Withdraw);
+            Assert.NotNull(transactionSend);
+            Assert.Equal(transferRequest.Amount, transactionSend.Amount);
+
+            var transactionReceive= dbContext.Transactions.FirstOrDefault(ts => ts.AccountId == accountReceive.Id && ts.Type == TransactionTypes.Deposit);
+            Assert.NotNull(transactionReceive);
+            Assert.Equal(transferRequest.Amount, transactionReceive.Amount);
+        }
+
+        private static async Task Create_Customer_And_Transfer_Arrange(CoreBankingServices services, Account accountSend, Account accountReceive)
+        {
+
+            var customerSend = new Customer
+            {
+                Id = Guid.NewGuid(),
+                Name = "locnso",
+                Address = "nam dinh"
+            };
+            var customerReceive = new Customer
+            {
+                Id = Guid.NewGuid(),
+                Name = "John Doe",
+                Address = "123 Main St"
+            };
+            var addedCustomerSend = CoreBankingApi.CreateCustomer(services, customerSend);
+            Assert.NotNull(addedCustomerSend);
+            var addedCustomerReceive = CoreBankingApi.CreateCustomer(services, customerReceive);
+            Assert.NotNull(addedCustomerReceive);
+
+            accountSend.CustomerId = customerSend.Id;
+            accountReceive.CustomerId = customerReceive.Id;
+
+            var addedAccountSend = await CoreBankingApi.CreateAccount(services, accountSend);
+            Assert.NotNull(addedAccountSend);
+
+            var addedAccountReceive = await CoreBankingApi.CreateAccount(services, accountReceive);
+            Assert.NotNull(addedAccountReceive);
+
+            services.DbContext.ChangeTracker.Clear();
+        }
     }
 }
